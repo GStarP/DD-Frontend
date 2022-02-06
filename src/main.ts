928a26cc9a761b33d4f49c1f620eee35095a229b
GStarP <171250605@smail.nju.edu.cn>
2022-02-06 12:21:35 +0800

    feat: 刷新保留用户信息

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,11 @@ app.use(router);
 app.use(store);
 
 app.mount('#app');
+
+// 刷新时保留用户信息
+window.onbeforeunload = () => {
+  sessionStorage.setItem(
+    'dd-store-user-info',
+    JSON.stringify(store.state.userInfo)
+  );
+};
--- src/plugins/vuex.ts
+++ src/plugins/vuex.ts
@@ -1,18 +1,25 @@
 import { createStore } from 'vuex';
 
+let userInfo: UserInfo = {
+  userId: 0,
+  userName: '',
+  password: '',
+  email: '',
+  phone: '',
+  gender: 0,
+  age: 0,
+  isFriend: false,
+  isBlacked: false
+};
+// 刷新时加载保留的用户信息
+const userInfoJson = sessionStorage.getItem('dd-store-user-info');
+if (userInfoJson !== null) {
+  userInfo = JSON.parse(userInfoJson);
+}
+
 const store = createStore({
   state: {
-    userInfo: {
-      userId: 0,
-      userName: '',
-      password: '',
-      email: '',
-      phone: '',
-      gender: 0,
-      age: 0,
-      isFriend: false,
-      isBlacked: false
-    } as UserInfo
+    userInfo
   },
   mutations: {
     userInfo(state, userInfo: UserInfo) {

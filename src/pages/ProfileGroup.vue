e7daad4edfebb5aa91d6a4c02bc8a531e6249ea1
GStarP <1700066935@qq.com>
2022-03-13 15:04:05 +0800

    feat: 邀请入群

--- src/pages/ProfileGroup.vue
+++ src/pages/ProfileGroup.vue
@@ -4,10 +4,8 @@
       <el-icon class="profile-group-back" :size="32" @click="back"
         ><ArrowLeft
       /></el-icon>
-      <div class="profile-group-title">
-        Group Profile
-      </div>
-      <span style="width: 32px; height: 1px;"></span>
+      <div class="profile-group-title">Group Profile</div>
+      <span style="width: 32px; height: 1px"></span>
     </div>
     <div class="profile-group__main">
       <div class="group-profile">
@@ -24,6 +22,34 @@
             <div class="group-profile-id">ID: {{ groupInfo.groupId }}</div>
           </div>
           <div class="group-profile__actions">
+            <!-- 邀请朋友 -->
+            <el-dialog v-model="inviteShow" title="Invite Friend" width="400px">
+              <el-table
+                :data="inviteFriendList"
+                @selection-change="handleSelectFriend"
+              >
+                <el-table-column type="selection" width="64"></el-table-column>
+                <el-table-column
+                  label="ID"
+                  property="friendId"
+                  width="64"
+                ></el-table-column>
+                <el-table-column
+                  label="Name"
+                  property="nickname"
+                ></el-table-column>
+              </el-table>
+              <el-button
+                class="invite-confirm"
+                type="primary"
+                @click="inviteSelectedFriends"
+                >INVITE</el-button
+              >
+            </el-dialog>
+            <el-button type="primary" @click="inviteShow = true"
+              >INVITE</el-button
+            >
+            <!-- 退出群组 -->
             <el-button type="danger" @click="quitGroup">{{
               isGroupMaster ? 'DISSOLVE' : 'QUIT'
             }}</el-button>
@@ -54,12 +80,12 @@
               <div class="group-members-item__id">ID: {{ m.userId }}</div>
             </div>
             <div class="group-members-item__action">
-              <!-- TODO 踢出成员 -->
-              <!-- <el-button
+              <!-- 踢出成员 -->
+              <el-button
                 v-if="isGroupMaster && uid !== m.userId"
                 @click="kick(m)"
                 >Kick</el-button
-              > -->
+              >
             </div>
           </div>
         </el-scrollbar>
@@ -77,6 +103,7 @@ import { useStore } from 'vuex';
 import { ElMessage, ElMessageBox } from 'element-plus';
 import { reqGroupInfo, reqQuitGroup } from '@/api/group';
 import { generateAvatarColor } from '@/utils/avatar';
+import { reqListFriend } from '@/api/friend';
 
 // 获取路由参数
 const route = useRoute();
@@ -115,6 +142,35 @@ const back = () => {
 /**
  * 群组信息
  */
+// 邀请朋友
+const inviteShow = ref(false);
+const friendList = ref<FriendBrief[]>([]);
+reqListFriend(uid.value).then((res) => {
+  if (res.code === 0) {
+    friendList.value = res.data;
+  }
+});
+const inviteFriendList = computed<FriendBrief[]>(() => {
+  const li = [];
+  const mems = groupInfo.value.members.map((m) => m.userId);
+  for (const f of friendList.value) {
+    if (!mems.includes(f.friendId)) {
+      li.push(f);
+    }
+  }
+  return li;
+});
+// 控制多选
+const selectedFriends = ref<FriendBrief[]>([]);
+const handleSelectFriend = (v: FriendBrief[]) => {
+  selectedFriends.value = v;
+};
+// TODO 请求邀请
+const inviteSelectedFriends = () => {
+  console.log(selectedFriends.value);
+  inviteShow.value = false;
+};
+
 // 解散/退出群组
 const quitGroup = () => {
   const keyword = computed(() => (isGroupMaster.value ? 'dissolve' : 'quit'));
@@ -138,14 +194,14 @@ const quitGroup = () => {
 };
 
 // 踢出用户
-// const kick = (u: UserBrief) => {
-//   ElMessageBox.confirm(
-//     `Are you sure to kick ${u.userName}?`,
-//     'Kick Member'
-//   ).then(() => {
-//     ElMessage.info('kicked');
-//   });
-// };
+const kick = (u: UserBrief) => {
+  ElMessageBox.confirm(
+    `Are you sure to kick ${u.userName}?`,
+    'Kick Member'
+  ).then(() => {
+    ElMessage.info('kicked');
+  });
+};
 </script>
 
 <style lang="scss">
@@ -182,7 +238,6 @@ const quitGroup = () => {
     padding: 36px 0;
   }
   .group-profile {
-    width: 400px;
     display: flex;
     flex-direction: column;
     &__main {
@@ -203,7 +258,7 @@ const quitGroup = () => {
       font-size: 15px;
     }
     &__actions {
-      margin-left: auto;
+      margin-left: 100px;
       display: flex;
       flex-direction: row;
       > i {
@@ -215,6 +270,14 @@ const quitGroup = () => {
       }
       > i:not(:first-child) {
         margin-left: 16px;
+      }
+
+      .el-dialog__body {
+        padding-top: 0;
+      }
+      .invite-confirm {
+        width: 100%;
+        margin-top: 16px;
       }
     }
   }
